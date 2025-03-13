2cb84397c73cafc98bc39c3dcdd3aef705d05dea
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-03-13 08:34:02 +0000

    editing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ sudo apt update && sudo apt install nodejs npm
 #install pm2 a production process mgr for node.js w built-in load balancer
 sudo npm install -g pm2
 #stop any instance of our app currently running
-pm2 stop simple_app
+pm2 stop simple_application
 #change dir into folder where app downloaded
 cd simple-app/
 #install app dependencies
 npm install
-#start app w process name simple_app using pm2
-pm2 start ./bin/www --name simple_app
+#start app w process name simple_application using pm2
+pm2 start ./bin/www --name simple_application
