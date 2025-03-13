7f276144378d508ba55958913c29154b4b46292b
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-03-13 07:57:27 +0000

    editing config.yml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,10 @@ sudo apt update && sudo apt install nodejs npm
 #install pm2 a production process mgr for node.js w built-in load balancer
 sudo npm install -g pm2
 #stop any instance of our app currently running
-pm2 stop simple_application
+pm2 stop simple_app
 #change dir into folder where app downloaded
 cd simple-app/
 #install app dependencies
 npm install
-echo $PRIVATE_KEY > privatekey.pem
-echo $SERVER > server.crt
-#start app w process name simple_application using pm2
-pm2 start ./bin/www --name simple_application
+#start app w process name simple_app using pm2
+pm2 start ./bin/www --name simple_app
